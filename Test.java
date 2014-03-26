b2077b2cbf6ec0d9988662a8ead3db6eea47174a
joyana <joyana84@gmail.com>
2014-03-26 22:00:45 +0900

    by ejkim

--- Test.java
+++ Test.java
@@ -13,6 +13,10 @@ public class Test {
 	
 	public void add(){
 		Test01 t1 = new Test01();
+		// 김은주는 이쁘다. 그러나 너무 이쁘다.
+		// 강광구는 요즘 디아블로에 빠졌다.
+		// 그만 해야한다..
+		// 그러나 계속한다.
 		Test01.test01_sub();
 	}
 	
